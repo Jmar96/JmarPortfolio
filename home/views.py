d7500be78a676858e8a8681436211da0065501a3
hilawnabangus <jaymaryugo90@gmail.com>
2023-06-22 19:45:44 +0800

    add Home
    Add Home/Welcome page and create a layout base page.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from datetime import datetime
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
+    extra_context = {'today': datetime.today()}
--- jmar/settings.py
+++ jmar/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'jmar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'static/layouts',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
